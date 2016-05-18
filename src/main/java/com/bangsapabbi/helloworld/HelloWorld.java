afc5c50aed05500a9222131987f30eb66cd21ca6
iluxa_com <iluxa_com@mail.ru>
2016-05-18 17:09:41 +0300

    Update HelloWorld.java

--- src/main/java/com/bangsapabbi/helloworld/HelloWorld.java
+++ src/main/java/com/bangsapabbi/helloworld/HelloWorld.java
@@ -3,6 +3,8 @@ package com.bangsapabbi.helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
     World world = new World();    
+    World worldToKill = new World();
+    kill(worldToKill);
     System.out.println(world.greet());
     }
 }
